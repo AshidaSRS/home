099d923e08497bdedace1f6b6e7ddee0dcf8a4b6
Ashida <ashida.shin@gmail.com>
2015-05-06 22:26:46 +0200

    .emacs.d\ arreglado

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,21 +12,6 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-;; Full bell diable
-(setq ring-bell-function 'ignore)
-
-;;Auto Fill mode
-(add-hook 'text-mode-hook 'auto-fill-mode)
-
-;; Hide menu-bar
-(menu-bar-mode -1)
-
-;; Hide scroll
-(scroll-bar-mode -1)
-
-;; Hide tool bar
-(tool-bar-mode -1)
-
 ;; Haskell mode
 ;; custom variables
 (custom-set-variables
@@ -42,13 +27,6 @@
  '(org-replace-disputed-keys nil)
  '(recentf-mode t)
  '(shift-select-mode nil))
-
-;; distinguished theme
-;;(load-theme 'distinguished t)
-
-;;moe-theme
-(require 'moe-theme)
-(moe-dark)
 
 ;; In every buffer, the line which contains the cursor will be fully
 ;; highlighted
@@ -128,139 +106,13 @@
 ;; For editing lisps
 (load "elisp-editing.el")
 
+;; For UI settings
+(load "ui.el")
+
+
 ;; Langauage-specific
 (load "setup-clojure.el")
 (load "setup-js.el")
-
-;; Tamaño de letra
-(set-face-attribute 'default nil :height 100)
-
-;;Ciao
-;;(load "/usr/lib/ciao/ciao-mode-init")
-
-;;lua-mode
-
-(setq ring-bell-function 'ignore)
-
-;;line-number
-(line-number-mode 1)
-
-;;Auto Fill mode
-(add-hook 'text-mode-hook 'auto-fill-mode)
-
-;; Hide menu-bar
-(menu-bar-mode -1)
-
-;; Hide scroll
-(scroll-bar-mode -1)
-
-;; Hide tool bar
-(tool-bar-mode -1)
-
-;; Haskell mode
-;; custom variables
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(TeX-PDF-mode t)
- '(delete-selection-mode t)
- '(ergoemacs-mode t)
- '(haskell-mode-hook (quote (turn-on-haskell-indent turn-on-haskell-doc interactive-haskell-mode)))
- '(org-CUA-compatible nil)
- '(org-replace-disputed-keys nil)
- '(recentf-mode t)
- '(shift-select-mode nil))
-
-;; distinguished theme
-;;(load-theme 'distinguished t)
-
-;;moe-theme
-(require 'moe-theme)
-(moe-dark)
-
-;; In every buffer, the line which contains the cursor will be fully
-;; highlighted
-(global-hl-line-mode 1)
-
-;; Clojure
-;; require or autoload paredit-mode
-(add-hook 'clojure-mode-hook 'paredit-mode)
-
-;; require or autoload smartparensq
-(add-hook 'clojure-mode-hook 'smartparens-strict-mode)
-
-;; Parenthesis mode
-(show-paren-mode 1)
-
-;; Tamaño de letra
-(set-face-attribute 'default nil :height 100)
-
-;;Ciao
-;;(load "/usr/lib/ciao/ciao-mode-init")
-
-;;lua-mode
-
-(setq ring-bell-function 'ignore)
-
-;;line-number
-(line-number-mode 1)
-
-;;Auto Fill mode
-(add-hook 'text-mode-hook 'auto-fill-mode)
-
-;; Hide menu-bar
-(menu-bar-mode -1)
-
-;; Hide scroll
-(scroll-bar-mode -1)
-
-;; Hide tool bar
-(tool-bar-mode -1)
-
-;; Haskell mode
-;; custom variables
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(TeX-PDF-mode t)
- '(delete-selection-mode t)
- '(ergoemacs-mode t)
- '(haskell-mode-hook (quote (turn-on-haskell-indent turn-on-haskell-doc interactive-haskell-mode)))
- '(org-CUA-compatible nil)
- '(org-replace-disputed-keys nil)
- '(recentf-mode t)
- '(shift-select-mode nil))
-
-;; distinguished theme
-;;(load-theme 'distinguished t)
-
-;;moe-theme
-(require 'moe-theme)
-(moe-dark)
-
-;; In every buffer, the line which contains the cursor will be fully
-;; highlighted
-(global-hl-line-mode 1)
-
-;; Clojure
-;; require or autoload paredit-mode
-(add-hook 'clojure-mode-hook 'paredit-mode)
-
-;; require or autoload smartparensq
-(add-hook 'clojure-mode-hook 'smartparens-strict-mode)
-
-;; Parenthesis mode
-(show-paren-mode 1)
-
-;; Tamaño de letra
-(set-face-attribute 'default nil :height 100)
-
-;;Ciao
-;;(load "/usr/lib/ciao/ciao-mode-init")
 
 ;; Lua mode
 (autoload 'lua-mode "lua-mode" nil t)
