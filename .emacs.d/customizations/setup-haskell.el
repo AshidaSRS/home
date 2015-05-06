1434cc3bb64d6126f69167fe7c284c8c8691a96a
Ashida <ashida.shin@gmail.com>
2015-05-06 23:46:11 +0200

    web added

---
+++ .emacs.d/customizations/setup-haskell.el
@@ -0,0 +1,15 @@
+;; Haskell mode
+;; custom variables
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(TeX-PDF-mode t)
+ '(delete-selection-mode t)
+ '(ergoemacs-mode t)
+ '(haskell-mode-hook (quote (turn-on-haskell-indent turn-on-haskell-doc interactive-haskell-mode)))
+ '(org-CUA-compatible nil)
+ '(org-replace-disputed-keys nil)
+ '(recentf-mode t)
+ '(shift-select-mode nil))
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,99 +3,61 @@
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives
-  '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/"))
-
+             '("tromey" . "http://tromey.com/elpa/") t)
 (add-to-list 'package-archives
-  '("marmalade" . "http://marmalade-repo.org/packages/"))
+  			 '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-;; Haskell mode
-;; custom variables
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(TeX-PDF-mode t)
- '(delete-selection-mode t)
- '(ergoemacs-mode t)
- '(haskell-mode-hook (quote (turn-on-haskell-indent turn-on-haskell-doc interactive-haskell-mode)))
- '(org-CUA-compatible nil)
- '(org-replace-disputed-keys nil)
- '(recentf-mode t)
- '(shift-select-mode nil))
 
-;; In every buffer, the line which contains the cursor will be fully
-;; highlighted
-(global-hl-line-mode 1)
-
-;; The packages you want installed. You can also install these
-;; manually with M-x package-install
-;; Add in your own as you wish:
-(defvar my-packages
-  '(;; makes handling lisp expressions much, much easier
-    ;; Cheatsheet: http://www.emacswiki.org/emacs/PareditCheatsheet
+;;packages to install
+(defvar my-packages'(
     paredit
-
-    ;; key bindings and code colorization for Clojure
-    ;; https://github.com/clojure-emacs/clojure-mode
     clojure-mode
-
-    ;; extra syntax highlighting for clojure
     clojure-mode-extra-font-locking
-
-    ;; integration with a Clojure REPL
-    ;; https://github.com/clojure-emacs/cider
     cider
-
-    ;; allow ido usage in as many contexts as possible. see
-    ;; customizations/navigation.el line 23 for a description
-    ;; of ido
+    ;; allow ido usage in as many contexts as possible.
+    ;; customizations/navigation.el line 23 
     ido-ubiquitous
-
     ;; Enhances M-x to allow easier execution of commands. Provides
     ;; a filterable list of possible commands in the minibuffer
-    ;; http://www.emacswiki.org/emacs/Smex
     smex
-
     ;; project navigation
     projectile
-
-    ;; colorful parenthesis matching
     rainbow-delimiters
-
     ;; edit html tags like sexps
     tagedit
-
     ;; git integration
-    magit))
-;; Adding this code will make Emacs enter yaml mode whenever you open
-;; a .yml file
-(add-to-list 'load-path "~/.emacs.d/vendor")
+    magit
+    moe-theme
+    scss-mode
+    yaml-mode
+    coffee-mode
+    web-mode
+    emmet-mode
+    ))
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
 
 
-;;;;
-;; Customization
-;;;;
+(add-to-list 'load-path "~/.emacs.d/extras")
 
-;; Add a directory to our load path so that when you `load` things
-;; below, Emacs knows where to look for the corresponding file.
+;;;;;;;;;;;;;;;;;;;
+;; Customization ;;
+;;;;;;;;;;;;;;;;;;;
+
+;;Directory for diferents conf files
 (add-to-list 'load-path "~/.emacs.d/customizations")
 
-;; Sets up exec-path-from-shell so that Emacs will use the correct
-;; environment variables
+;; Sets up exec-path-from-shell so that Emacs will use the correct environment variables
 (load "shell-integration.el")
 
-;; These customizations make it easier for you to navigate files,
-;; switch buffers, and choose options from the minibuffer.
+;; These customizations are for navigation between buffers, etc.
 (load "navigation.el")
-
-;; These customizations change the way emacs looks and disable/enable
-;; some user interface elements
-;;(load "ui.el")
 
 ;; These customizations make editing a bit nicer.
 (load "editing.el")
@@ -106,16 +68,15 @@
 ;; For editing lisps
 (load "elisp-editing.el")
 
-;; For UI settings
+;; some user interface elements
 (load "ui.el")
-
 
 ;; Langauage-specific
 (load "setup-clojure.el")
 (load "setup-js.el")
-
-;; Lua mode
-(autoload 'lua-mode "lua-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
-(add-hook 'lua-mode-hook 'turn-on-font-lock)
-(setq lua-indent-level 2)
+(load "setup-haskell.el")
+(load "setup-lua.el")
+(load "setup-css.el")
+(load "setup-ruby.el")
+(load "setup-yaml.el")
+(load "setup-web.el")
