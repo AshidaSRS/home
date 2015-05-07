43b94b8628f2c8a48a1312f89411030dec94d71b
AshidaSRS <ashida.shin@gmail.com>
2015-05-07 02:22:00 +0200

    test files added

---
+++ Clojure/prueba.clj
@@ -0,0 +1,10 @@
+(ns clojure-noob.core
+  (gen-class))
+
+(defn -main [& args] 
+  (println "hello world!"))
+(println "Cleanliness is next to godliness")
+
+(defn train
+  []
+  (println "Choo choo!"))
